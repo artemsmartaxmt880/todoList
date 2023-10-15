39856379e0e13d19a37556ce4b0a8e24f33b5955
artemsmartaxmt880 <artemsmartaxmt880@mail.ru>
2023-10-15 15:48:40 +0300

    Fix

--- js/main.js
+++ js/main.js
@@ -1,189 +1,159 @@
 const form = document.getElementById('form');
 const taskInput = document.getElementById('taskInput');
 const tasksList = document.getElementById('tasksList');
+const tasksListItem = document.querySelectorAll('.tasksList__item')
 const emptyList = document.getElementById('emptyList');
+const boxDone = document.querySelector('.tasks__boxDone')
 const btnRemoveAll = document.querySelector('.btn__remove');
 const btnRemoveDone = document.querySelector('.btn__removeDone')
 form.addEventListener('submit', addTask);
-tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
+tasksList.addEventListener('click', deleteTask);
+tasksDoneList.addEventListener('click', deleteDoneTask);
+tasksDoneList.addEventListener('click', moveDoneTask);
 btnRemoveAll.addEventListener('click', removeAllTask);
 btnRemoveDone.addEventListener('click', removeDoneTask);
-// ! массив, каторый содержит в себе все задачи
+
 let tasks = [];
-// ! возвращаем из LocalStorage данные 
+let tasksDone = [];
 if (localStorage.getItem('tasks')) {
     tasks = JSON.parse(localStorage.getItem('tasks'))
 };
-// ! и добавляем их в массив
+if (localStorage.getItem('tasksDone')) {
+    tasksDone = JSON.parse(localStorage.getItem('tasksDone'))
+};
 tasks.forEach(function (task) {
     renderTask(task);
 });
-checkEmptyList();
+tasksDone.forEach(function (task) {
+    renderDoneTask(task);
+});
+checkEmptyList()
+function checkEmptyList() {
+    if (tasksList.children.length > 1) {
+        emptyList.classList.add('hidden');
+    }
+    else {
+        emptyList.classList.remove('hidden');
+    }
+    if (tasksDoneList.children.length > 0) {
+        boxDone.classList.remove('hidden');
+    }
+    else {
+        boxDone.classList.add('hidden');
+    }
+
+}
 function addTask(event) {
-    // после submit страница перезагружается(стандартное поведение формы), поэтому
-    // обьявляем параметр (event) и в собках отменяем ему это поведение
-    event.preventDefault();
-    // достаем текст из Input, value - это значение Input
-    const taskText = taskInput.value;
-    // запрещаю вводить пустое поле
+    event.preventDefault()
+    const taskText = taskInput.value
     if (taskText.length == 0) return;
-    // ! создаем объект описывающий эту задачу
-    const newTask = {
-        // ! текущая дата в ms 
+    const task = {
         id: Date.now(),
         text: taskText,
-        done: true,
-    };
-    // ! добавляем задачу в массив со всеми задачами
-    // ! push добавляет в конец массива
-    tasks.push(newTask);
-    saveToLocalStorage()
-    // ! меняем task на newTask
-    renderTask(newTask);
-    // ощищаем поле ввода поле надатия кнопки и сразу же возвращаем на него фокус
+    }
+    tasks.push(task)
+    renderTask(task)
     taskInput.value = '';
     taskInput.focus();
-    // ? // скрываем пустой лист и картинку, если в листе больше 1ого элемента <li>
-    // ? // пишем условие
-    // ? if (tasksList.children.length > 1) {
-    // ?     emptyList.classList.add('hidden');
-    // ? }
-    checkEmptyList();
-};
+    checkEmptyList()
+    saveTolocalStorage()
+}
 function deleteTask(event) {
-    // если я захочу поставить img на button, на в css надо указать (pointer-events: none;) ей
-    // =====================================================
     if (event.target.dataset.action !== 'delete') return;
-    const parentNode = event.target.closest('li');
-    // ! удаляем задачу из данных
-    // ! определяем id задачи
-    const id = Number(parentNode.id);
-    // * // ! находим id задачи в массиве
-    // * // ! function(...) переменная, по которой обращаемся к каждому элементу массива 
-    // * const index = tasks.findIndex(function (task) {
-    // *     // ! тот элемент массива, который вернется с true, будит считатся найденным
-    // *     // ! если не найдет, то вернет -1
-    // *     return task.id === id;
-    // * });
-    // * // ! splice начиная с  .. , он убрал .. элемент (массивы)
-    // * tasks.splice(index, 1);
-    // ! те элементы массива, которые вернутся с true, попадут в новый массив
-    // ! чтоб это сработало переменная должна быть объявлена через let
+    const item = event.target.closest('li');
+    const id = Number(item.id);
     tasks = tasks.filter(function (task) {
         return task.id !== id;
     });
-    saveToLocalStorage()
-    parentNode.remove();
-    // ?показываем пустой лист и картинку, если в листе 1 элемент <li>
-    // ?пишем условие
-    // ?if (tasksList.children.length === 1) {
-    // ?    emptyList.classList.remove('hidden');
-    // ?}
+    item.remove()
+    saveTolocalStorage()
+    checkEmptyList();
+};
+function deleteDoneTask(event) {
+    if (event.target.dataset.action !== 'delete') return;
+    const item = event.target.closest('li');
+    const id = Number(item.id);
+    tasksDone = tasksDone.filter(function (task) {
+        return task.id !== id;
+    });
+    item.remove()
+    saveTolocalStorage()
     checkEmptyList();
 };
 function doneTask(event) {
-    const emptyImage = document.querySelector('.tasksList__image');
-    if (event.target.emptyImage) return;
-    // если я захочу поставить img на button, на в css надо указать (pointer-events: none;) ей
-    // *if (event.target.dataset.action !== 'done') return;
     if (event.target.dataset.action === 'delete') return;
-    // ! поставил, чтоб не выдавало ошибку при нажании на пустую картинку
-    if (tasksList.children.length === 1) return;
-    const parentNode = event.target.closest('li');
-    if (parentNode === null) return;
-    const id = +parentNode.id;   
-    // ! находим id задачи в массиве
-    // ! function(...) переменная, по которой обращаемся к каждому элементу массива 
-    // ! find возвращает ссыклу на элемент, тоесть мы можем работать с task
-    // ! который ссылается на его же
-    const task = tasks.find(function (task) {
-        // ! тот элемент массива, который вернется с true, будит считатся найденным
-        // ! если не найдет, то вернет -1
+    if (event.target === emptyList) return;
+    if (event.target === tasksList) return;
+    const item = event.target
+    const id = +item.id
+    const index = tasks.findIndex((task) => {
         return task.id === id;
-    });
-    task.done = !task.done;
-    const done = tasks.filter(task => task.done === false);
-    tasks.sort((a, b) => {
-        if (a.done > b.done) {
-            return -1;
-        } else {
-            return 1;
-        };
-    });
-    const inProg = tasks.filter(task => task.done === true);
-    tasks.sort((a, b) => {
-        if (a.done > b.done) {
-            return -1;
-        } else {
-            return 1;
-        };
-    });
-    const taskTitle = parentNode.querySelector('.tasksList__text');
-    taskTitle.classList.toggle('done');
-    parentNode.remove();
-    renderTask(task);
-    saveToLocalStorage()
-};
-function removeDoneTask(event) {
-    // ! удаляю из массива done 
-    const done = tasks.filter(task => task.done === true)
-    tasks.slice(done);
-    tasks = done;
-    let doneList = tasksList.querySelectorAll('.done');
+    })
+    tasksDone.push(tasks[index]);
+    tasks.splice([index], 1)
+    item.remove();
+    const task = tasksDone[tasksDone.length - 1]
+    renderDoneTask(task)
+    checkEmptyList()
+    saveTolocalStorage()
+}
+function moveDoneTask(event) {
+    if (event.target.dataset.action === 'delete') return;
+    if (event.target === tasksDoneList) return;
+    const item = event.target
+    const id = +item.id
+    const index = tasksDone.findIndex((task) => {
+        return task.id === id;
+    })
+    tasks.push(tasksDone[index]);
+    tasksDone.splice([index], 1)
+    item.remove();
+    const task = tasks[tasks.length - 1]
+    renderTask(task)
+    checkEmptyList()
+    saveTolocalStorage()
+}
+function removeDoneTask() {
+    tasksDone = []
+    const doneList = tasksDoneList.querySelectorAll('.tasksList__item')
     for (let i = 0; i < doneList.length; i++) {
-        doneList[i].closest('li').remove();
+        doneList[i].remove();
     }
-    saveToLocalStorage()
-        // *чтоб не вылезала еще 1 пустая иконка
-    if (tasksList.children.length < 1) {
-        checkEmptyList();
-    }
+    saveTolocalStorage()
+    checkEmptyList()
 }
-function removeAllTask(event) {
-    // нахожу все <li>, задаю каждому task и удаляю(просто remove, как раньше, не сработало)
-    tasksList.querySelectorAll('.tasksList__item').forEach(task => task.remove());
-    // присваиваю [], чтоб не было 0
-    tasks = [];
-    saveToLocalStorage()
-    // *чтоб не вылезала еще 1 пустая иконка
-    if (tasksList.children.length < 1) {
-        checkEmptyList();
+function removeAllTask() {
+    tasksDone = []
+    const doneList = tasksDoneList.querySelectorAll('.tasksList__item')
+    for (let i = 0; i < doneList.length; i++) {
+        doneList[i].remove();
     }
+    tasks = []
+    const list = tasksList.querySelectorAll('.tasksList__item')
+    for (let i = 0; i < list.length; i++) {
+        list[i].remove();
+    }
+    saveTolocalStorage()
+    checkEmptyList()
 }
-function checkEmptyList() {
-    if (tasks.length === 0) {
-        const emptyListHTML = `
-        <li class="tasksList__emptyItem" id="emptyList">
-        <div class="tasksList__image">
-            <img id="emptyImage" src="img/grunge.png" alt="empty">
-        </div>
-    </li>
-        `;
-        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
-    }
-    if (tasks.length > 0) {
-        const emptyListElement = document.getElementById('emptyList');
-        // ! условие ? if true(удаляем): if false(возвращаем null);(тернальный апператор)
-        // ! т.к если не найдется emptyListElement, то он будит равен null
-        emptyListElement ? emptyListElement.remove() : null;
-    }
-}
-function saveToLocalStorage() {
-    // ! 1 агументт, по какому ключу, 2ой что(массив или объект надо
-    // ! транформировать в Json строку)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+function saveTolocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('tasksDone', JSON.stringify(tasksDone));
 }
 function renderTask(task) {
-    const cssClass = task.done ? "tasksList__text" : "tasksList__text done";
-    // ? Эту кнопку я убрал
-    // ?<button class="btn__done" data-action="done">&#10003;</button>
     const taskHTML = `
     <li class="tasksList__item" id="${task.id}">
-    <div class="${cssClass}">${task.text}</div>
+    <div class="tasksList__text">${task.text}</div>
         <button class="btn__delete" data-action="delete">&#10008;</button>
     </li>`;
     tasksList.insertAdjacentHTML('beforeend', taskHTML);
 }
-
-// console.log(tasks)
+function renderDoneTask(task) {
+    const taskHTML = `
+    <li class="tasksList__item" id="${task.id}">
+    <div class="tasksList__text done">${task.text}</div>
+        <button class="btn__delete" data-action="delete">&#10008;</button>
+    </li>`;
+    tasksDoneList.insertAdjacentHTML('beforeend', taskHTML);
+}
